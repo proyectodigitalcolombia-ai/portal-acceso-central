85691d0601bd932d902ec79965cde0e0cb8bddb8
proyectodigitalcolombia-ai <proyectodigitalcolombia@gmail.com>
2026-03-01 14:07:15 -0500

    Update database.js

--- database.js
+++ database.js
@@ -7,22 +7,20 @@ async function setupDb() {
         driver: sqlite3.Database
     });
 
-    // Añadimos la columna 'rol'
     await db.exec(`
         CREATE TABLE IF NOT EXISTS usuarios (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE,
             password TEXT,
-            rol TEXT
+            rol TEXT,
+            ultima_conexion TEXT
         )
     `);
 
-    // Creamos al Administrador principal con rol 'admin'
     const userExist = await db.get('SELECT * FROM usuarios WHERE username = ?', ['admin']);
     if (!userExist) {
         await db.run('INSERT INTO usuarios (username, password, rol) VALUES (?, ?, ?)', 
         ['admin', '1234', 'admin']);
-        console.log("✅ Super Administrador creado");
     }
     return db;
 }
